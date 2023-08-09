f859d2f1a0f0682a4c245e02feffe1a30a7ba744
ozgucdlg <ozgucdalga@gmail.com>
2023-08-09 12:51:01 -0400

    Generated business layer  with AccountManager class and AccountService  interface. Created related methods.

--- src/main/java/com/restApi/bankApp/business/abstracts/AccountService.java
+++ src/main/java/com/restApi/bankApp/business/abstracts/AccountService.java
@@ -1,4 +1,16 @@
 package com.restApi.bankApp.business.abstracts;
 
+import com.restApi.bankApp.entities.Account;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
 public interface AccountService {
+
+    Account createAccount(Account account);
+    Optional<Account> getAccount(int id);
+    Account deposit(int id, double amount);
+    Account withdraw(int id, double amount);
+
 }
--- src/main/java/com/restApi/bankApp/business/concretes/AccountManager.java
+++ src/main/java/com/restApi/bankApp/business/concretes/AccountManager.java
@@ -1,4 +1,40 @@
 package com.restApi.bankApp.business.concretes;
 
-public class AccountManager {
+import com.restApi.bankApp.business.abstracts.AccountService;
+import com.restApi.bankApp.dataAccess.AccountRepository;
+import com.restApi.bankApp.entities.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class AccountManager implements AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+    @Override
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+    @Override
+    public Optional<Account> getAccount(int id) {
+
+        return accountRepository.findById(id);
+    }
+    @Override
+    public Account deposit(int id, double amount) {
+        Account account= getAccount(id).orElseThrow(()-> new RuntimeException("Account not found"));
+        account.setBalance(account.getBalance()+amount);
+        return accountRepository.save(account);
+    }
+
+    @Override
+    public Account withdraw(int id, double amount) {
+        Account account= getAccount(id).orElseThrow(()-> new RuntimeException("Äccount not found"));
+        if(account.getBalance() < amount){
+            throw new RuntimeException("Your funds are not enough");
+
+        }
+        account.setBalance(account.getBalance() - amount);
+        return accountRepository.save(account);
+    }
 }
